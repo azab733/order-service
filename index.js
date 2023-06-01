cfc53db619d6c781a9fa8993d0ed8954a9a20898
Adam Zabloudil <adampzab@gmail.com>
2023-06-01 10:07:11 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+app.get('/', (req, res) => {
+  pool.query('SELECT NOW()', (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error executing query');
+    } else {
+      res.send(`Current time: ${result.rows[0].now}`);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
